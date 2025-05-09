7bc85c19f4c4349a44ba7b3a36fcc94ec3eee1b9
Marco ZECCHETTO <zecchetto@NB-ZECCHETTO.iiasa.ac.at>
2025-05-09 17:26:19 +0200

    Aggiunto test personalizzato

--- tests/unita/test_unita.py
+++ tests/unita/test_unita.py
@@ -1,5 +1,8 @@
+import re
+
 import pint
 import pint.testing
+import pytest
 
 from unit_test_prj.coscente_unita import somma_unit_aware
 
@@ -15,3 +18,16 @@ def test_add_unit_aware():
     res = somma_unit_aware(a, b)
 
     pint.testing.assert_equal(res, comp)
+
+
+def test_errore():
+    a = Q(1.0, "m")
+    b = Q(1.0, "kg")
+
+    with pytest.raises(
+        TypeError,
+        match=re.escape(
+            "Cannot convert from 'meter' ([length]) to 'kilogram' ([mass])"
+        ),
+    ):
+        somma_unit_aware(a, b)
